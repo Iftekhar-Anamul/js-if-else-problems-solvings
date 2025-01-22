4eab54b264b69f9c91b0422d8da6a5087caab278
Iftekhar Anamul <iftekharanamul39@gmail.com>
2025-01-22 21:18:04 +0600

    problem 2 added

--- problem-2.js
+++ problem-2.js
@@ -1,3 +1,25 @@
-// ২. দতনটি সংখ্যার মজযয সেজেজয় েড়টি খ্ ুঁজ  বের িরা 
-// দতনটি সংখ্যা ইনপুি নাও। যদি িথমটি সেজেজয় েড় হয়, তাহজে কনজসাজে "Number 1 is largest" 
-// দিন্ট করজে, অনযথায় দিতীয় ো ততীয়টি বেক করজে। 
+// ২. দতনটি সংখ্যার মজযয সেজেজয় েড়টি খ্ ুঁজ  বের িরা
+// দতনটি সংখ্যা ইনপুি নাও। যদি িথমটি সেজেজয় েড় হয়, তাহজে কনজসাজে "Number 1 is largest"
+// দিন্ট করজে, অনযথায় দিতীয় ো ততীয়টি বেক করজে।
+
+var numberOne = 8;
+
+var numberTwo = 8;
+
+var numberThree = 10;
+
+if (numberOne > numberTwo) {
+  if (numberOne > numberThree) {
+    console.log("Number 1 is largest");
+  } else if (numberOne === numberThree) {
+    console.log("numberOne and NumberThree is Equal.");
+  } else {
+    console.log("number Tree is largest");
+  }
+} else if (numberTwo < numberThree) {
+  console.log("number three is largest");
+} else if (numberOne === numberTwo) {
+  console.log("numberOne and NumberTwo is Equal.");
+} else {
+  console.log("numberTwo is largest");
+}
